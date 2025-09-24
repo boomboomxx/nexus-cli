3e9cceaba6683e64adb5f023f4cbbcb4f71837c4
xx <xx@123.com>
2025-09-24 10:11:05 +0800

    init: Init

--- upload.sh
+++
@@ -1,5 +0,0 @@
-go build -o nexus-cli .
-curl -u admin:admin123 --upload-file nexus-cli "https://nexus.szistech.com/repository/raw-szis-releases/sziscloud/nexus-cli/1.0.1/nexus-cli"
-if [ $? eq 0]; then
-  echo 'upload success.'
-fi
